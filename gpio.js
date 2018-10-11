e1ee76cb574eeffa0bd88d8878ec23541f67cd06
Marty Kane <marty.kane@gmail.com>
2018-10-11 12:37:43 -0500

    Add mock gpio module for development

--- gpio.js
+++ gpio.js
@@ -1,20 +1,65 @@
-const fakePinStatuses = {
-    1: 'Low',
-    2: 'Low',
-    3: 'Low',
-    4: 'Low',
-    5: 'Low'
+const Gpio = loadGpioModule();
+
+const OutputMode = {
+  mode: Gpio.OUTPUT
 };
 
-const getAllPinStatuses = () => {
-    return fakePinStatuses;
+const pin = {
+  '12': new Gpio(12, OutputMode),
+  '13': new Gpio(13, OutputMode),
+  '18': new Gpio(18, OutputMode)
 };
 
-const getPinStatus = (id) => {
-    return fakePinStatuses[id];
-};
+redPin = pin['13'];
+greenPin = pin['12'];
+bluePin = pin['18'];
+
+function getAllPinStatuses() {
+  const result = {};
+  const allPinIds = Object.keys(pin);
+
+  allPinIds.forEach(id => result[id] = getPinStatus(id));
+
+  return result;
+}
+
+function getPinStatus(pinId) {
+  const gpioForPin = pin[pinId];
+
+  return {
+    mode: gpioForPin.getMode(),
+    level: gpioForPin.digitalRead(),
+    pwm: gpioForPin.getPwmDutyCycle()
+  };
+}
+
+function getColor() {
+  //get PWM duty cycles and convert to color codes
+}
+
+function setColor(redVal, greenVal, blueVal) {
+
+}
+
+function mockGpio() {
+  return {
+    getMode: () => 'OUTPUT',
+    digitalRead: () => 0,
+    getPwmDutyCycle: () => 0
+  }
+}
+mockGpio.OUTPUT = 'output';
+
+function loadGpioModule() {
+  if (process.env['TEST'])
+    return mockGpio;
+  else
+    return require('pigpio').Gpio;
+}
 
 module.exports = {
-    getAllPinStatuses,
-    getPinStatus
+  getAllPinStatuses,
+  getColor,
+  getPinStatus,
+  setColor
 };
