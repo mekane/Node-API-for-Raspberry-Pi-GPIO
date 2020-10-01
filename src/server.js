32bb55092a7a095485782698a92e7f019013697b
Marty Kane <marty.kane@gmail.com>
2020-10-01 11:15:52 -0500

    Reorganize the code into a src/ directory that exports a server module

---
+++ src/server.js
@@ -0,0 +1,154 @@
+#!/usr/bin/env node
+
+const accessLogger = require('./accessLogger');
+const express = require('express');
+const bodyParser = require('body-parser');
+const gpio = require('./gpio');
+
+//for accepts-content matching
+const html = 'text/html';
+const json = 'application/json';
+
+function initialize(port) {
+
+    const routes = [
+        {
+            method: 'get',
+            path: '/',
+            description: 'Lists all available routes',
+            handler: getAllRouteDescriptions
+        },
+        {
+            method: 'get',
+            path: '/pin',
+            description: 'Lists all pins and their state',
+            handler: getAllPins
+        },
+        {
+            method: 'get',
+            path: '/pin/:id',
+            description: 'Returns the status of the specified pin',
+            handler: getPin
+        },
+        {
+            method: 'put',
+            path: '/pin/:id',
+            description: 'Sets the status of a pin to on or off',
+            handler: setPin
+        },
+        {
+            method: 'get',
+            path: '/color',
+            description: 'Gets the RGB value currently set',
+            handler: getColor
+        },
+        {
+            method: 'put',
+            path: '/color',
+            description: 'Sets the RGB value using JSON {red:<val>, green:<val>, blue:<val>}',
+            handler: setColor
+        }
+    ];
+
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(accessLogger);
+    app.use(express.static('public'));
+    app.engine('mustache', require('mustache-express')());
+    app.set('view engine', 'mustache')
+
+    gpio.setColor(0, 0, 0);
+    gpio.setPinStatus(23, 0);
+    setupRouting();
+    startServer();
+
+    function setupRouting() {
+        routes.forEach(route => app[route.method](route.path, route.handler));
+    }
+
+    function startServer() {
+        app.listen(port, () => console.log(`GPIO app listening on port ${port}!`));
+    }
+
+
+    function getAllRouteDescriptions(req, res) {
+        const routeInfo = routes.map(route => ({
+            method: route.method,
+            path: route.path,
+            description: route.description
+        }));
+
+        res.format({
+            html: () => res.render('allRouteDescriptions', {
+                routes: routeInfo
+            }),
+            json: () => res.json(routeInfo)
+        });
+    }
+
+    function getAllPins(req, res) {
+        const allPins = gpio.getAllPinStatuses();
+
+        res.format({
+            html: () => res.render('allPinsStatus', {
+                pins: allPinStatusesForHtml()
+            }),
+            json: () => res.json(allPins)
+        });
+
+        function allPinStatusesForHtml() {
+            return Object.keys(allPins).map(id => {
+                const pin = allPins[id];
+                pin['id'] = id;
+                return pin;
+            })
+        }
+    }
+
+    function getPin(req, res) {
+        const pinId = req.params['id'];
+        const pinStatus = gpio.getPinStatus(pinId);
+
+        res.format({
+            html: () => res.render('pinStatus', {
+                id: pinId,
+                pin: pinStatus
+            }),
+            json: () => res.json(pinStatus)
+        });
+    }
+
+    function setPin(req, res) {
+        const pinId = req.params['id'];
+        const body = req.body || {};
+        const newState = body.state;
+
+        const result = gpio.setPinStatus(pinId, newState);
+
+        res.send(result);
+    }
+
+    function getColor(req, res) {
+        const rgb = gpio.getColor();
+
+        res.format({
+            html: () => res.render('rgbColor', {color: rgb}),
+            json: () => res.json(rgb)
+        })
+    }
+
+    function setColor(req, res) {
+        const body = req.body || {};
+        const red = body.red || 0;
+        const green = body.green || 0;
+        const blue = body.blue || 0;
+
+        gpio.setColor(red, green, blue);
+
+        res.send(`setColor(${red}, ${green}, ${blue})`);
+    }
+}
+
+module.exports = {
+    initialize
+}
